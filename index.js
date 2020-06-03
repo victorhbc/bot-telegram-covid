2a1ecf8f5e87cebbe1699ea192e2d8fbc1f47bdc
victorhbc <victorhugoboninic@gmail.com>
2020-06-03 17:12:15 -0300

    make it so much better

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-'use strict'
+"use strict";
 
-const http = require('http');
+const http = require("http");
 
-const port = (process.env.PORT || 5000)
+const port = process.env.PORT || 5000;
 
-http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json'
-  });
-  res.write(JSON.stringify({
-    name: 'Covid-Puc',
-    ver: '0.0.1'
-  }))
-  res.end()
-}).listen(port)
+require("./bot.js")();
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.write(
+      JSON.stringify({
+        name: "Covid-Puc",
+        ver: "0.0.1",
+      })
+    );
+    res.end();
+  })
+  .listen(port);
 
 // const express = require("express");
-
 
 // const app = express();
 
 // app.listen(process.env.PORT || 443);
 
-// 2020-06-03T01:16:22.583353+00:00 
+// 2020-06-03T01:16:22.583353+00:00
