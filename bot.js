dc964072e4ef63efa048c7fcd66d6cb5a3c93e71
victorhbc <victorhugoboninic@gmail.com>
2020-06-03 19:14:12 -0300

    make it so much better

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ module.exports = function () {
 
     const chatId = msg.chat.id;
 
-    resp = `O Brasil possui ${JSON.stringify(this.a.data.data.deaths)} casos de covid confirmado, casos ativos e  mortes`;
+    resp = `O Brasil possui um total de ${JSON.stringify(this.a.data.data.cases)} casos de covid-19 ativos`;
     // ${JSON.stringify(this.a.data.data.confirmed)} ${JSON.stringify(this.a.data.data.deaths)}
     bot.sendMessage(chatId, resp);
   });
