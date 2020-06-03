658790aa75cbc8f08b9c0755f81047edaeb8e375
victorhbc <victorhugoboninic@gmail.com>
2020-06-03 16:52:42 -0300

    make it so much better

---
+++ bot.js
@@ -0,0 +1,79 @@
+'use strict'
+
+const axios = require("axios").default;
+
+const TelegramBot = require(`node-telegram-bot-api`);
+
+module.exports = () => {
+
+
+  const TOKEN = `1234732971:AAEq5CwrwdkbVjIxnMPe_9ksIwjySyrcSAI`;
+
+  const bot = new TelegramBot(TOKEN, {
+    polling: true,
+  });
+
+  bot.onText(/\/1/, (msg, match) => {
+    axios
+      .get("https://covid19-brazil-api.now.sh/api/report/v1")
+      .then((response) => (this.a = response))
+      .catch((error) => console.log(error));
+
+    const chatId = msg.chat.id;
+
+    console.log(this.a.data.data[0]);
+
+    resp = `O primeiro estado brasileiro com mais casos de corona virus e ${this.a.data.data[0].state}, o mesmo possui ${this.a.data.data[0].cases} casos registrados`;
+
+    bot.sendMessage(chatId, resp);
+  });
+
+  bot.onText(/\/2/, (msg, match) => {
+    axios
+      .get("https://covid19-brazil-api.now.sh/api/report/v1")
+      .then((response) => (this.a = response))
+      .catch((error) => console.log(error));
+
+    const chatId = msg.chat.id;
+
+    console.log(this.a.data.data[1]);
+
+    resp = `O primeiro estado brasileiro com mais casos de corona virus e ${this.a.data.data[1].state}, o mesmo possui ${this.a.data.data[1].cases} casos registrados`;
+
+    bot.sendMessage(chatId, resp);
+  });
+
+  bot.onText(/\/3/, (msg, match) => {
+    axios
+      .get("https://covid19-brazil-api.now.sh/api/report/v1")
+      .then((response) => (this.a = response))
+      .catch((error) => console.log(error));
+
+    const chatId = msg.chat.id;
+
+    console.log(this.a.data.data[2]);
+
+    resp = `O primeiro estado brasileiro com mais casos de corona virus e ${this.a.data.data[2].state}, o mesmo possui ${this.a.data.data[2].cases} casos registrados`;
+
+    bot.sendMessage(chatId, resp);
+  });
+
+  bot.onText(/\/brasil/, (msg, match) => {
+    axios
+      .get("https://covid19-brazil-api.now.sh/api/report/v1")
+      .then((response) => (this.a = response))
+      .catch((error) => console.log(error));
+
+    const chatId = msg.chat.id;
+
+    console.log(this.a.data.data[2]);
+
+    let resp = `Lista:\n`;
+
+    for (let index = 0; index < 26; index++) {
+      resp = `${resp} ${this.a.data.data[index].state}:  ${this.a.data.data[index].cases} casos, ${this.a.data.data[index].deaths} mortes\n`;
+    }
+
+    bot.sendMessage(chatId, resp);
+  });
+}
--- index.js
+++ index.js
@@ -1,78 +1,25 @@
-const express = require("express");
-const axios = require("axios").default;
+'use strict'
 
-const TelegramBot = require(`node-telegram-bot-api`);
+const http = require('http');
 
-const TOKEN = `1234732971:AAEq5CwrwdkbVjIxnMPe_9ksIwjySyrcSAI`;
+const port = (process.env.PORT || 5000)
 
-const bot = new TelegramBot(TOKEN, {
-  polling: true,
-});
+http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.write(JSON.stringify({
+    name: 'Covid-Puc',
+    ver: '0.0.1'
+  }))
+  res.end()
+}).listen(port)
 
-bot.onText(/\/1/, (msg, match) => {
-  axios
-    .get("https://covid19-brazil-api.now.sh/api/report/v1")
-    .then((response) => (this.a = response))
-    .catch((error) => console.log(error));
+// const express = require("express");
 
-  const chatId = msg.chat.id;
 
-  console.log(this.a.data.data[0]);
+// const app = express();
 
-  resp = `O primeiro estado brasileiro com mais casos de corona virus e ${this.a.data.data[0].state}, o mesmo possui ${this.a.data.data[0].cases} casos registrados`;
+// app.listen(process.env.PORT || 443);
 
-  bot.sendMessage(chatId, resp);
-});
-
-bot.onText(/\/2/, (msg, match) => {
-  axios
-    .get("https://covid19-brazil-api.now.sh/api/report/v1")
-    .then((response) => (this.a = response))
-    .catch((error) => console.log(error));
-
-  const chatId = msg.chat.id;
-
-  console.log(this.a.data.data[1]);
-
-  resp = `O primeiro estado brasileiro com mais casos de corona virus e ${this.a.data.data[1].state}, o mesmo possui ${this.a.data.data[1].cases} casos registrados`;
-
-  bot.sendMessage(chatId, resp);
-});
-
-bot.onText(/\/3/, (msg, match) => {
-  axios
-    .get("https://covid19-brazil-api.now.sh/api/report/v1")
-    .then((response) => (this.a = response))
-    .catch((error) => console.log(error));
-
-  const chatId = msg.chat.id;
-
-  console.log(this.a.data.data[2]);
-
-  resp = `O primeiro estado brasileiro com mais casos de corona virus e ${this.a.data.data[2].state}, o mesmo possui ${this.a.data.data[2].cases} casos registrados`;
-
-  bot.sendMessage(chatId, resp);
-});
-
-bot.onText(/\/brasil/, (msg, match) => {
-  axios
-    .get("https://covid19-brazil-api.now.sh/api/report/v1")
-    .then((response) => (this.a = response))
-    .catch((error) => console.log(error));
-
-  const chatId = msg.chat.id;
-
-  console.log(this.a.data.data[2]);
-
-  let resp = `Lista:\n`;
-
-  for (let index = 0; index < 26; index++) {
-    resp = `${resp} ${this.a.data.data[index].state}:  ${this.a.data.data[index].cases} casos, ${this.a.data.data[index].deaths} mortes\n`;
-  }
-
-  bot.sendMessage(chatId, resp);
-});
-
-const app = express();
-
-app.listen(process.env.PORT || 443);
+// 2020-06-03T01:16:22.583353+00:00 
